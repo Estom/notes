a70f328d979e05666f41092d0569ab6210c0b660
estomm <yinkanglong@163.com>
2021-03-28 00:14:24 +0800

    简直offer

---
+++ C++/C++基础/11.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a = 0b0010;
+    cout<<hex<<a<<endl;
+    return 0 ;
+}
--- C++/标准库/5.cpp
+++ C++/标准库/5.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<sstream>
 #include<string>
+#include<vector>
 using namespace std;
 
 int main(){
@@ -15,10 +16,32 @@ int main(){
     
     // char* helo;
     // s.getline(helo,10,',');
-    size_t m;
-    string s="123avicfee";
-    int a = stoi(s,&m,10);
-    cout<<a<<endl;
-    cout<<m<<endl;
+    // size_t m;
+    // string s="123avicfee";
+    // int a = stoi(s,&m,10);
+    // cout<<a<<endl;
+    // cout<<m<<endl;
+
+    string strs ="  adb   dai fei af";
+    
+    string pattern = " ";
+    size_t start_pos=0;
+    size_t end_pos = strs.find(pattern,start_pos);
+    vector<string> vec;
+    string temp;
+    while (end_pos != string::npos)
+    {   cout<<start_pos<<"--"<<end_pos<<endl;
+        if(start_pos!=end_pos){
+            temp = strs.substr(start_pos,end_pos-start_pos);
+            vec.push_back(temp);
+        }
+        start_pos = end_pos+1;
+        end_pos = strs.find(pattern,start_pos);
+        
+    }
+    vec.push_back(strs.substr(start_pos,strs.size()-start_pos));
+    for(auto a : vec){
+        cout<<"name:"<<a<<":end"<<endl;
+    }
     return 0;
 }
