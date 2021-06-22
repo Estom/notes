039a220259f278023ac9093fe3ce0c0bc42d967a
Estom <yinkanglong@163.com>
2021-06-22 13:59:28 +0800

    修改内容

--- Go/project/go.mod
+++ Go/project/go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	github.com/ramya-rao-a/go-outline v0.0.0-20200117021646-2a048b4510eb // indirect
+	github.com/smartystreets/goconvey v1.6.4 // indirect
 	golang.org/x/tools v0.1.1 // indirect
 )
---
+++ Go/project/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	})
+
+	http.ListenAndServe(":80", nil)
+
+	file, err := os.Create("writeAt.txt")
+
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	file.WriteString("Golang中文社区——这里是多余")
+	n, err := file.WriteAt([]byte("Go语言中文网"), 24)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(n)
+}
